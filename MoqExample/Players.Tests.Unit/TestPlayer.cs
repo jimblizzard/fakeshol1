b3dac1aa0892fe4e1485cb4b8a2b9b2cf57b9986
Jim Blizzard <jimblizzard@hotmail.com>
2018-05-03 16:03:10 -0400

    initial

---
+++ MoqExample/Players.Tests.Unit/TestPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Players;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Players.Tests.Unit
+{
+    [TestClass]
+    public class TestPlayer
+    { 
+        // Should receive an exception, because the name is an empty string.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_CreateNewPlayer_EmptyName()
+        { 
+            Player player = Player.CreateNewPlayer("");
+        }
+
+        // Should still receive an exception, because the name is an empty string.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_CreateNewPlayer_EmptyName_MockedDataMapper()
+        {
+             
+            // THIS is how Moq creates an object that implements the IPlayerDataMapper interface
+            var mock = new Mock<IPlayerDataMapper>();
+
+            Player player = Player.CreateNewPlayer("", mock.Object);
+        }
+
+        // Should receive an exception, because we set the mock PlayerDataMapper 
+        // to say the player name already exists in the database.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_CreateNewPlayer_AlreadyExistsInDatabase()
+        {
+            var mock = new Mock<IPlayerDataMapper>();
+             
+            // Intercept the call to PlayerNameExistsInDatabase() and return our own value
+            mock.Setup(x => x.PlayerNameExistsInDatabase(It.IsAny<string>())).Returns(true);
+
+            Player player = Player.CreateNewPlayer("Test", mock.Object);
+        }
+
+        // Should succeed, because we set the mock PlayerDataMapper 
+        // to say the player name does not already exist in the database.
+        // Also, when it calls the mock InsertNewPlayerIntoDatabase,
+        // the mock mapper will not need a database running.
+        [TestMethod]
+        public void Test_CreateNewPlayer_DoesNotAlreadyExistInDatabase()
+        { 
+            var mock = new Mock<IPlayerDataMapper>();
+
+            // Intercept the call to PlayerNameExistsInDatabase() and return our own value
+            mock.Setup(x => x.PlayerNameExistsInDatabase(It.IsAny<string>())).Returns(false);
+
+            Player player = Player.CreateNewPlayer("Test", mock.Object);
+
+            Assert.AreEqual("Test", player.Name);
+            Assert.AreEqual(0, player.ExperiencePoints);
+            Assert.AreEqual(10, player.Gold);
+        }
+
+    }
+}
---
+++ MoqExample/Players/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+///  Reference: https://scottlilly.com/creating-mock-objects-for-c-unit-tests-using-moq/
+
+
+/// </summary>
+namespace Players
+{
+    public class Player
+    { 
+        public string Name { get; private set; } 
+        public int ExperiencePoints { get; private set; } 
+        public int Gold { get; private set; } 
+
+        public Player(string name, int experiencePoints, int gold)
+        { 
+            Name = name;
+            ExperiencePoints = experiencePoints;
+            Gold = gold;
+        } 
+
+        // NOTICE how we optionally can pass the IPlayerDataMapper to this method
+        public static Player CreateNewPlayer(string name, IPlayerDataMapper playerDataMapper = null)
+        { 
+            // If a PlayerDataMapper was not passed in, use a real one.
+            // This allows us to pass in a "mock" PlayerDataMapper (for testing),
+            // but use a real PlayerDataMapper, when running the program.
+            if (playerDataMapper == null)
+            {
+                playerDataMapper = new PlayerDataMapper();
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Player name cannot be empty.");
+            }
+
+            // Throw an exception if there is already a player with this name in the database.
+            if (playerDataMapper.PlayerNameExistsInDatabase(name))
+            {
+                throw new ArgumentException("Player name already exists.");
+            }
+
+            // Add the player to the database.
+            playerDataMapper.InsertNewPlayerIntoDatabase(name);
+
+            return new Player(name, 0, 10);
+        }
+
+    }
+}
